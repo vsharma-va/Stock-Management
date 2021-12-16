190e159d7c133fccb976c69e92905887ba61e738
Vaibhav Sharma <yavsvaibhav@gmail.com>
2021-12-16 17:27:12 +0530

    Now pdfs are named as (current date time + name of the buyer)

--- src/main/kotlin/Document.kt
+++ src/main/kotlin/Document.kt
@@ -7,8 +7,9 @@ import com.itextpdf.layout.element.*
 import com.itextpdf.layout.property.TabAlignment
 import com.itextpdf.layout.property.TextAlignment
 import com.itextpdf.layout.property.UnitValue
+import javax.print.Doc
 
-class Document {
+class Document(private val data: Data) {
     private lateinit var companyNameAndInvoice: Paragraph
     private lateinit var add1AndDate: Paragraph
     private lateinit var add2AndInvoiceNum: Paragraph
@@ -23,54 +24,64 @@ class Document {
     private lateinit var table: Table
 
 
-    private val pdf = PdfDocument(PdfWriter("F:\\IDEs\\Kotlin\\Stock-Management\\src\\data\\document.pdf"))
-    private val document = Document(pdf)
+    private lateinit var pdf: PdfDocument
+    private lateinit var document: Document
 
 
     fun generateTemplate(yourCompanyName: String, addLine1: String, date: String, invoiceNumber: Int, phnNumber: String, custId: String,
                         faxNum: String, dueDate: String, billToName: String, companyName: String, compAdd: String,
-                        compPhnNum: String){
-        companyNameAndInvoice = Paragraph(yourCompanyName).setFontSize(20f).setTextAlignment(TextAlignment.LEFT).setBackgroundColor(
-            ColorConstants.LIGHT_GRAY)
-        companyNameAndInvoice.setMargins(0f, 0f, 0f, 0f)
-        companyNameAndInvoice.add(Tab())
-        companyNameAndInvoice.addTabStops(TabStop(520f, TabAlignment.RIGHT))
-        companyNameAndInvoice.add("INVOICE").setFontSize(30f).setTextAlignment(TextAlignment.RIGHT)
+                        compPhnNum: String): Boolean{
+        val fileName: MutableList<String> = data.addInvoiceToTable(billToName)
+        if (fileName[1].toBoolean()){
+            pdf = PdfDocument(PdfWriter("""F:\\IDEs\\Kotlin\\Stock-Management\\src\\data\\pdf\\(${fileName[0]}).pdf"""))
+            document = Document(pdf)
+            companyNameAndInvoice = Paragraph(yourCompanyName).setFontSize(20f).setTextAlignment(TextAlignment.LEFT).setBackgroundColor(
+                ColorConstants.LIGHT_GRAY)
+            companyNameAndInvoice.setMargins(0f, 0f, 0f, 0f)
+            companyNameAndInvoice.add(Tab())
+            companyNameAndInvoice.addTabStops(TabStop(520f, TabAlignment.RIGHT))
+            companyNameAndInvoice.add("INVOICE").setFontSize(30f).setTextAlignment(TextAlignment.RIGHT)
 
-        add1AndDate = Paragraph("$addLine1").setFontSize(7f).setTextAlignment(TextAlignment.LEFT)
-        add1AndDate.setMargins(0f, 0f, 0f, 4f)
-        add1AndDate.add(Tab())
-        add1AndDate.addTabStops(TabStop(1000f, TabAlignment.RIGHT))
-        add1AndDate.add("DATE: $date").setFontSize(10f).setTextAlignment(TextAlignment.RIGHT)
+            add1AndDate = Paragraph("$addLine1").setFontSize(7f).setTextAlignment(TextAlignment.LEFT)
+            add1AndDate.setMargins(0f, 0f, 0f, 4f)
+            add1AndDate.add(Tab())
+            add1AndDate.addTabStops(TabStop(1000f, TabAlignment.RIGHT))
+            add1AndDate.add("DATE: $date").setFontSize(10f).setTextAlignment(TextAlignment.RIGHT)
 
-        add2AndInvoiceNum = Paragraph("[City, ST ZIP]").setFontSize(7f).setTextAlignment(TextAlignment.LEFT)
-        add2AndInvoiceNum.setMargins(0f, 0f, 0f, 4f)
-        add2AndInvoiceNum.add(Tab())
-        add2AndInvoiceNum.addTabStops(TabStop(1000f, TabAlignment.RIGHT))
-        add2AndInvoiceNum.add("INVOICE #$invoiceNumber").setFontSize(10f).setTextAlignment(TextAlignment.RIGHT)
+            add2AndInvoiceNum = Paragraph("[City, ST ZIP]").setFontSize(7f).setTextAlignment(TextAlignment.LEFT)
+            add2AndInvoiceNum.setMargins(0f, 0f, 0f, 4f)
+            add2AndInvoiceNum.add(Tab())
+            add2AndInvoiceNum.addTabStops(TabStop(1000f, TabAlignment.RIGHT))
+            add2AndInvoiceNum.add("INVOICE #$invoiceNumber").setFontSize(10f).setTextAlignment(TextAlignment.RIGHT)
 
-        phnNumAndCustId = Paragraph("Phone: $phnNumber").setFontSize(7f).setTextAlignment(TextAlignment.LEFT)
-        phnNumAndCustId.setMargins(0f, 0f, 0f, 4f)
-        phnNumAndCustId.add(Tab())
-        phnNumAndCustId.addTabStops(TabStop(1000f, TabAlignment.RIGHT))
-        phnNumAndCustId.add("CUSTOMER ID: $custId").setFontSize(10f).setTextAlignment(TextAlignment.RIGHT)
+            phnNumAndCustId = Paragraph("Phone: $phnNumber").setFontSize(7f).setTextAlignment(TextAlignment.LEFT)
+            phnNumAndCustId.setMargins(0f, 0f, 0f, 4f)
+            phnNumAndCustId.add(Tab())
+            phnNumAndCustId.addTabStops(TabStop(1000f, TabAlignment.RIGHT))
+            phnNumAndCustId.add("CUSTOMER ID: $custId").setFontSize(10f).setTextAlignment(TextAlignment.RIGHT)
 
-        fax = Paragraph("fax: $faxNum").setFontSize(7f).setTextAlignment(TextAlignment.LEFT)
-        fax.setMargins(0f, 0f, 0f, 4f)
-        fax.add(Tab())
-        fax.addTabStops(TabStop(1000f, TabAlignment.RIGHT))
-        fax.add("DUE DATE: $dueDate").setFontSize(10f).setTextAlignment(TextAlignment.RIGHT)
+            fax = Paragraph("fax: $faxNum").setFontSize(7f).setTextAlignment(TextAlignment.LEFT)
+            fax.setMargins(0f, 0f, 0f, 4f)
+            fax.add(Tab())
+            fax.addTabStops(TabStop(1000f, TabAlignment.RIGHT))
+            fax.add("DUE DATE: $dueDate").setFontSize(10f).setTextAlignment(TextAlignment.RIGHT)
 
-        billTo = Paragraph("BILL TO").setFontSize(20f).setTextAlignment(TextAlignment.LEFT).setBackgroundColor(ColorConstants.LIGHT_GRAY)
-        billTo.setMargins(0f, 0f, 0f, 4f)
-        name = Paragraph("$billToName").setFontSize(10f).setTextAlignment(TextAlignment.LEFT)
-        name.setMargins(0f, 0f, 0f, 4f)
-        compName = Paragraph("$companyName").setFontSize(10f).setTextAlignment(TextAlignment.LEFT)
-        compName.setMargins(0f, 0f, 0f, 4f)
-        streetAdd = Paragraph("$compAdd").setFontSize(10f).setTextAlignment(TextAlignment.LEFT)
-        streetAdd.setMargins(0f, 0f, 0f, 4f)
-        phnNum = Paragraph("$compPhnNum").setFontSize(10f).setTextAlignment(TextAlignment.LEFT)
-        phnNum.setMargins(0f, 0f, 15f, 4f)
+            billTo = Paragraph("BILL TO").setFontSize(20f).setTextAlignment(TextAlignment.LEFT).setBackgroundColor(ColorConstants.LIGHT_GRAY)
+            billTo.setMargins(0f, 0f, 0f, 4f)
+            name = Paragraph("$billToName").setFontSize(10f).setTextAlignment(TextAlignment.LEFT)
+            name.setMargins(0f, 0f, 0f, 4f)
+            compName = Paragraph("$companyName").setFontSize(10f).setTextAlignment(TextAlignment.LEFT)
+            compName.setMargins(0f, 0f, 0f, 4f)
+            streetAdd = Paragraph("$compAdd").setFontSize(10f).setTextAlignment(TextAlignment.LEFT)
+            streetAdd.setMargins(0f, 0f, 0f, 4f)
+            phnNum = Paragraph("$compPhnNum").setFontSize(10f).setTextAlignment(TextAlignment.LEFT)
+            phnNum.setMargins(0f, 0f, 15f, 4f)
+            return true
+        }
+        else{
+            println("Aborted")
+            return false
+        }
     }
 
     fun generateTable(billList: MutableList<MutableList<String>>, cgst: Float, igst: Float){
