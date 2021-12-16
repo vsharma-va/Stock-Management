1287888106450e340571238459576424a26a11a1
Vaibhav Sharma <yavsvaibhav@gmail.com>
2021-12-16 12:37:58 +0530

    Added functions to create bill

--- src/main/kotlin/UserControl.kt
+++ src/main/kotlin/UserControl.kt
@@ -1,7 +1,8 @@
-import Data
-import java.lang.NumberFormatException
+import java.text.SimpleDateFormat
+import java.util.*
+import kotlin.NumberFormatException
 
-class UserControl(private val data: Data){
+class UserControl(private val data: Data, private val document: Document){
     fun whatToDo()
     {
         if(data.getStatus()){
@@ -26,13 +27,27 @@ class UserControl(private val data: Data){
                 val foundValue: String = data.retrieveStockName(userInputIndex)
                 println("Found: $foundValue")
             }
-//            else if(userInput == 4){
-//
-//            }
+            else if(userChoice == 4){
+                var billItem: MutableList<MutableList<String>> = collectBillData()
+                val templateData: MutableList<String> = getDataForDocumentTemplate()
+                document.generateTemplate(templateData[0], templateData[1], templateData[2], templateData[3].toInt(),
+                templateData[4], templateData[5], templateData[6], templateData[7], templateData[8], templateData[9],
+                templateData[10], templateData[11])
+
+                print("Enter CGST %: ")
+                val cgstPercentage: Float = readLine()!!.toFloat()
+
+                print("Enter IGST %: ")
+                val igstPercentage: Float = readLine()!!.toFloat()
+
+                document.generateTable(billItem, cgstPercentage, igstPercentage)
+                document.writeToPdf()
+            }
         }
     }
 
     private fun addRecords(){
+        var userInputPrice:Double?
         do{
             println("Enter -1 to exit")
             print("Enter Name Of The Stock: ")
@@ -45,7 +60,16 @@ class UserControl(private val data: Data){
             if (userInputAmount == -1) {
                 break
             }
-            data.addRecords(userInputName, userInputAmount)
+
+            do {
+                print("Enter the price : ")
+                userInputPrice = readLine()!!.toDoubleOrNull()
+                if (userInputPrice == null) println("Not a valid number, try again")
+            }
+            while (userInputPrice == null)
+
+            val userInputPriceDouble: Double = userInputPrice.toDouble()
+            data.addRecords(userInputName, userInputAmount, userInputPriceDouble)
         }while(true)
     }
 
@@ -69,5 +93,91 @@ class UserControl(private val data: Data){
             returnedMap = data.retrieveStockAmount(index=index)
         }
         return returnedMap
+    }
+
+    private fun collectBillData(): MutableList<MutableList<String>>{
+        var returnArray: MutableList<MutableList<String>> = mutableListOf()
+        do {
+            var map: MutableMap<String, Int>
+            var groupedItems: MutableList<String> = mutableListOf()
+            println("Enter -1 to exit anytime")
+            print("Enter the name or the index of the stock: ")
+            val userInput: String = readLine().toString()
+
+            if (userInput == "-1"){
+                break
+            }
+
+            map = data.retrieveStockAmount(name=userInput)
+            println("Found: $map")
+
+            print("Enter the Quantity: ")
+            val userInputAmount: Int = Integer.valueOf(readLine())
+
+            if (userInputAmount == -1){
+                break
+            }
+
+            groupedItems = data.selectItemsForBill(userInputAmount, name=userInput)
+            returnArray.add(groupedItems)
+        } while(true)
+
+        return returnArray
+    }
+
+    private fun getDataForDocumentTemplate(): MutableList<String>{
+        var returnArray: MutableList<String> = mutableListOf()
+
+        print("Enter Your Company's name: ")
+        val compName: String = readLine().toString()
+
+        print("Enter Your Company's Address Line 1: ")
+        val addLine1: String = readLine().toString()
+
+        val sdf = SimpleDateFormat("dd/M/yyyy")
+        val currentDate = sdf.format(Date()).toString()
+
+        // create database to store all the invoices
+        // take its index as the invoice number
+        val invoiceNumber: Int = 1
+
+        print("Enter Your Company's Phone Number: ")
+        val phnNumber: String = readLine().toString()
+
+        print("Enter Customer ID: ")
+        val custId: String = readLine().toString()
+
+        print("Enter Fax Number: (press enter to leave it empty)")
+        val faxNum: String = readLine().toString()
+
+        print("Enter Due Date: (dd/mm/yyyy) ")
+        val dueDate: String = readLine().toString()
+
+        print("Enter the buyers name: ")
+        val billToName: String = readLine().toString()
+
+        print("Enter the buyers company name: (Press enter to leave it empty) ")
+        val companyName: String = readLine().toString()
+
+        print("Enter the buyers/ company's address: ")
+        val companyAdd: String = readLine().toString()
+
+        print("Enter buyers/ company's phone number: ")
+        val compPhnNum: String = readLine().toString()
+
+        returnArray.add(compName)
+        returnArray.add(addLine1)
+        returnArray.add(currentDate)
+        returnArray.add(invoiceNumber.toString())
+        returnArray.add(phnNumber)
+        returnArray.add(custId)
+        returnArray.add(faxNum)
+        returnArray.add(dueDate)
+        returnArray.add(billToName)
+        returnArray.add(companyName)
+        returnArray.add(companyAdd)
+        returnArray.add(compPhnNum)
+
+        return returnArray
     }
 }
