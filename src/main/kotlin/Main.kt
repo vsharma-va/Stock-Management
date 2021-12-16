5dde964fc8e8a4509b341de142acbcda80030e84
Vaibhav Sharma <yavsvaibhav@gmail.com>
2021-12-16 17:27:56 +0530

    Now Document class requires Data object

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,6 @@
 fun main(args: Array<String>){
     val dataClass: Data = Data()
-    val document: Document = Document()
+    val document: Document = Document(dataClass)
     val userClass: UserControl = UserControl(dataClass, document)
 
     userClass.whatToDo()
